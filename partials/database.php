ca81d1df28ab8fa19e446b05dca73df4165c67eb
eliaraguso <elia.raguso@gmail.com>
2021-10-22 10:01:33 +0200

    modificato database.php

--- partials/database.php
+++ partials/database.php
@@ -3,61 +3,61 @@ $database = [
 	[
 		'title' => 'New Jersey',
 		'author' => 'Bon Jovi',
-		'year' => 1988,
+		'year' => "1988",
 		'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
 	],
 	[
 		'title' => 'Live at Wembley 86',
 		'author' => 'Queen',
-		'year' => 1992,
+		'year' => "1992",
 		'poster' => 'https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg'
 	],
 	[
 		'title' => 'Ten\'s Summoner\'s Tales',
 		'author' => 'Sting',
-		'year' => 1993,
+		'year' => "1993",
 		'poster' => 'https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg'
 	],
 	[
 		'title' => 'Steve Gadd band',
 		'author' => 'Steve Gadd Band',
-		'year' => 2018,
+		'year' => "2018",
 		'poster' => 'https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg'
 	],
 	[
 		'title' => 'Brave new World',
 		'author' => 'Iron Maiden',
-		'year' => 2000,
-		'poster' => 'https://i.ebayimg.com/images/g/1GwAAOSwFytaKr7l/s-l300.jpg'
+		'year' => "2000",
+		'poster' => 'https://upload.wikimedia.org/wikipedia/en/0/03/Iron_Maiden_-_Brave_New_World.jpg'
 	],
 	[
 		'title' => 'One more car, one more rider',
 		'author' => 'Eric Clapton',
-		'year' => 2002,
+		'year' => "2002",
 		'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg'
 	],
 	[
-		'author' => "Deep Purple",
-		'poster' => "https://images-na.ssl-images-amazon.com/images/I/51rggtPgmRL.jpg",
 		'title' => "Made in Japan",
-		'year' => "1972"
+		'author' => "Deep Purple",
+		'year' => "1972",
+		'poster' => "https://images-na.ssl-images-amazon.com/images/I/51rggtPgmRL.jpg",
 	],
 	[
-		'author' => "Metallica",
-		'poster' => "https://images-na.ssl-images-amazon.com/images/I/81r3FVfNG3L._SY355_.jpg",
 		'title' => "And Justice for All",
-		'year' => "1988"
+		'author' => "Metallica",
+		'year' => "1988",
+		'poster' => "https://images-na.ssl-images-amazon.com/images/I/81r3FVfNG3L._SY355_.jpg",
 	],
 	[
-		'author' => "Dave Weckl",
-		'poster' => "https://img.discogs.com/KOBsqQwKiNKH-q927oHMyVdDzSo=/fit-in/596x596/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-6406665-1418464475-6120.jpeg.jpg",
 		'title' => "Hard Wired",
-		'year' => "1994"
+		'author' => "Dave Weckl",
+		'year' => "1994",
+		'poster' => "https://img.discogs.com/KOBsqQwKiNKH-q927oHMyVdDzSo=/fit-in/596x596/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-6406665-1418464475-6120.jpeg.jpg",
 	],
 	[
-		'author' => "Michael Jacjson",
-		'poster' => "https://m.media-amazon.com/images/I/71K9CbNZPsL._SS500_.jpg",
 		'title' => "Bad",
-		'year' => "1987"
+		'author' => "Michael Jacjson",
+		'year' => "1987",
+		'poster' => "https://m.media-amazon.com/images/I/71K9CbNZPsL._SS500_.jpg",
 	]
 ];
