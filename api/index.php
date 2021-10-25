5f7b4a27d1f1f8c926af9a181a6ea575598a4277
eliaraguso <elia.raguso@gmail.com>
2021-10-25 10:09:15 +0200

    Aggiunta select per scelta genere degli album

--- api/index.php
+++ api/index.php
@@ -3,7 +3,26 @@ require __DIR__ . "/../partials/database.php";
 
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
-echo(json_encode($database));
 
+if (!empty($_GET['genres'])) {
+    $genres = [];
+    foreach($database as $album) {
+        if (in_array($album['genre'], $genres) == false) {
+            $genres[] = $album['genre'];
+        }
+    }
+    echo json_encode($genres);
+} elseif (!empty($_GET['genre'])) {
+    $genre = $_GET['genre'];
+    $albumFiltered = [];
+    foreach($database as $album) {
+        if ($genre == $album['genre']) {
+            $albumFiltered[] = $album;
+        }
+    }
+    echo json_encode($albumFiltered);
+} else {
+    echo json_encode($database);
+}
 
 ?>
--- js/main.js
+++ js/main.js
@@ -1,18 +1,31 @@
 const app = new Vue({
     el: '#root',
     data: {
-        albums : [],
+        albums: [],
+        genres: [],
+        genreSelected: ''
     },
     created() {
-        // Make a request for a user with a given ID
         axios.get('http://localhost/47-PHPAjaxDischi/php-ajax-dischi/api/index.php')
+        .then((response) => {
+            this.albums = response.data;
+        });
+
+        axios.get('http://localhost/47-PHPAjaxDischi/php-ajax-dischi/api/index.php?genres=genres')
+        .then((response) => {
+            this.genres = response.data;
+        });
+    },
+    methods: {
+        albumFilter() {
+            axios.get('http://localhost/47-PHPAjaxDischi/php-ajax-dischi/api/index.php',  {
+                params: {
+                    genre: this.genreSelected
+                }
+            })
             .then((response) => {
-                // handle success
                 this.albums = response.data;
-            })
-            .catch(function (error) {
-                // handle error
-                console.log(error);
-            })
+            });
+        }
     }
 })
